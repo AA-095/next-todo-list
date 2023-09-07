da160692c59a1da94703298a8b009dde985de098
藤井　香凜 <in-fujiikarin@LAB-N2900R.local>
2023-09-07 18:43:58 +0900

    buttonStyleを削除

--- src/components/CloseButton/index.tsx
+++ src/components/CloseButton/index.tsx
@@ -6,14 +6,11 @@ type Props = {
 } ;
 
 export const CloseButton: FC<Props> = ({ onClick, ...restProps }) => {
-  const buttonStyle = {};
-
   return (
     <button
       type="button"
       className={styles.closeButton}
       onClick={onClick}
-      style={buttonStyle}
       {...restProps}
     >
       &times; {}
